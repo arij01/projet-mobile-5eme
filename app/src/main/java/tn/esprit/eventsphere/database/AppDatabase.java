f0eb6f5001c7a63d4eecb8c7c2a06578fd6ad33c
arij <m'hiri.arij@esprit.tn>
2024-11-12 16:57:41 +0100

    updated app database

--- app/src/main/java/tn/esprit/eventsphere/database/AppDatabase.java
+++ app/src/main/java/tn/esprit/eventsphere/database/AppDatabase.java
@@ -7,15 +7,18 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import tn.esprit.eventsphere.dao.CommentDao;
+import tn.esprit.eventsphere.dao.EventDao;
 import tn.esprit.eventsphere.dao.ShareDao;
 import tn.esprit.eventsphere.entity.Comment;
+import tn.esprit.eventsphere.entity.Event;
 import tn.esprit.eventsphere.entity.Share;
 
-@Database(entities = {Comment.class, Share.class}, version = 2, exportSchema = false)
+@Database(entities = {Comment.class, Share.class, Event.class}, version = 2, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase instance;
     public abstract CommentDao commentDao();
     public abstract ShareDao shareDao();
+    public abstract EventDao eventDao();
 
     public static AppDatabase getAppDatabase(Context context) {
         if (instance == null) {
